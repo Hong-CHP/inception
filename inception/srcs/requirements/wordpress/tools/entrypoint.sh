7fad1ac47ddce53409379dde467879b1c7d76e4f
Hong-CHP <93changhong@gmail.com>
2026-02-02 17:16:02 +0100

    add_wp_config_install

--- inception/srcs/requirements/wordpress/tools/entrypoint.sh
+++ inception/srcs/requirements/wordpress/tools/entrypoint.sh
@@ -19,4 +19,30 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 	sed -i "s/localhost/${MYSQL_HOST}/" /var/www/html/wp-config.php
 fi
 
+cd /var/www/html
+
+if ! wp core is-installed --allow-root; then
+	ADMIN_USER=$(grep WP_ADMIN_USER /run/secrets/credentials | cut -d '=' -f2)
+	ADMIN_PWD=$(grep WP_ADMIN_PASSWORD /run/secrets/credentials | cut -d '=' -f2)
+	WPU_USR=$(grep WP_USR /run/secrets/credentials | cut -d '=' -f2)
+    WPU_PWD=$(grep WP_PWD /run/secrets/credentials | cut -d '=' -f2)
+    WPU_EMAIL=$(grep WP_EMAIL /run/secrets/credentials | cut -d '=' -f2)
+
+	echo "WordPress database not found. Installing..."
+	wp core install --allow-root \
+		--url="${DOMAIN_NAME}" \
+		--title="Inception" \
+		--admin_user="${ADMIN_USER}" \
+		--admin_password="${ADMIN_PWD}" \
+		--admin_email="hporta-c@student.42.fr" \
+		--skip-email
+
+	wp user create --allow-root \
+		"${WPU_USR}" "${WPU_EMAIL}" \
+		--user_pass="${WPU_PWD}" \
+        --role=author
+fi
+
+echo "WordPress launch succesfully!"
+
 exec "$@"
