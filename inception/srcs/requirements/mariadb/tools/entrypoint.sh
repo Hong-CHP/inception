74fa4a91b6c666231e95a2e86e67d10967cde460
Hong <93changhong@gmail.com>
2026-02-02 04:35:20 -0500

    permission_issus

--- inception/srcs/requirements/mariadb/tools/entrypoint.sh
+++ inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -5,23 +5,25 @@ set -eu
 #-u if env var not defined exit
 
 #make sure mariadb is not initialized before
-if [ ! -d "/var/lib/mysql/mysql "]; then
+if [ ! -d "/var/lib/mysql/mysql" ]; then
 	echo "Initializing MariaDB..."
 
 	#initialized mariadb with mysql user and define data directory
 	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 
 	#initialzed in bootstrap way
-	mariadb --user=mysql --bootstrap <<EOF
+	mysqld --bootstrap <<EOF
 FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '$(cat /run/secrets/db_password)';
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '$(cat /run/secrets/db_password)';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$(cat /run/secrets/db_root_password)';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'localhost';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$(cat /run/secrets/db_root_password)';
 FLUSH PRIVILEGES;
 EOF
 
 	echo "MariaDB initialized"
 fi
 
-exec "$@"
+exec mysqld --user=mysql
