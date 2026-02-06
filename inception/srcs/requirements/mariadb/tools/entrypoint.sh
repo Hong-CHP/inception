e9e8354c162c8244a5bedba02006662be1b1e192
Hong-CHP <93changhong@gmail.com>
2026-02-07 00:56:43 +0100

    detach_sh_file

--- inception/srcs/requirements/mariadb/tools/entrypoint.sh
+++ inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 
-mkdir -p /docker-entrypoint-initdb.d
-
-while [ ! -f /run/secrets/db_password ]; do
-	sleep 0.1
-done
-
-export MYSQL_PASSWORD=$(cat"$MYSQL_PASSWORD_FILE")
-
-echo "my pwd is : ${MYSQL_PASSWORD}"
-
-cat <<EOF > /docker-entrypoint-initdb.d/init.sql
-CREATE DATABASE IF NOT EXISTS '${MYSQL_DATABASE}';
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON '${MYSQL_DATABASE}'.* TO '${MYSQL_USER}'@'%';
-FLUSH PRIVILEGES;
-EOF
-
-# bash /usr/local/bin/init.sh
+bash /usr/local/bin/init.sh
 
 mysqld_safe &
 
