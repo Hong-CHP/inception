036bed81441ba01a415d4fbd6512406f65cdc656
Hong <93changhong@gmail.com>
2026-02-06 12:01:17 -0500

    always_rpob

--- inception/srcs/requirements/mariadb/tools/entrypoint.sh
+++ inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -11,7 +11,7 @@ done
 export MYSQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 
 if [ -f /docker-entrypoint-initdb.d/init.sql ];then
-    mysql -u root < /docker-entrypoint-initdb.d/init.sql
+    mysql -u root -p'${MYSQL_ROOT_PASSWORD}' < /docker-entrypoint-initdb.d/init.sql
 fi
 
 wait
--- inception/srcs/requirements/mariadb/tools/init.sh
+++ inception/srcs/requirements/mariadb/tools/init.sh
@@ -2,11 +2,16 @@
 
 mkdir -p /docker-entrypoint-initdb.d
 
-export MYSQL_PASSWORD=$(cat /run/secrets/db_password)
+while [ ! -f /run/secrets/db_password ]; do
+	sleep 0.1
+done
+
+export MYSQL_PASSWORD="$(tr -d '\n' < /run/secrets/db_password)"
+export MYSQL_ROOT_PASSWORD ="(tr -d '\n' < /run/secrets/db_password)"
 
 cat <<EOF > /docker-entrypoint-initdb.d/init.sql
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE DATABASE IF NOT EXISTS '${MYSQL_DATABASE}';
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+GRANT ALL PRIVILEGES ON '${MYSQL_DATABASE}'.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
