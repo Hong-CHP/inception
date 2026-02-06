f455a33c9e5d5147f41093c4894a2402731dec30
Hong-CHP <93changhong@gmail.com>
2026-02-06 18:52:36 +0100

    combine_init_file_mariadb

--- inception/srcs/requirements/mariadb/tools/entrypoint.sh
+++ inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+mkdir -p /docker-entrypoint-initdb.d
+
+while [ ! -f /run/secrets/db_password ]; do
+	sleep 0.1
+done
+
+export MYSQL_PASSWORD=$(cat /run/secrets/db_password)
+
+cat <<EOF > /docker-entrypoint-initdb.d/init.sql
+CREATE DATABASE IF NOT EXISTS '${MYSQL_DATABASE}';
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON '${MYSQL_DATABASE}'.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+
 bash /usr/local/bin/init.sh
 
 mysqld_safe &
--- inception/srcs/requirements/mariadb/tools/init.sh
+++ inception/srcs/requirements/mariadb/tools/init.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
 
-mkdir -p /docker-entrypoint-initdb.d
-
-while [ ! -f /run/secrets/db_password ]; do
-	sleep 0.1
-done
-
-export MYSQL_PASSWORD="$(tr -d '\n' < /run/secrets/db_password)"
-export MYSQL_ROOT_PASSWORD ="(tr -d '\n' < /run/secrets/db_password)"
-
-cat <<EOF > /docker-entrypoint-initdb.d/init.sql
-CREATE DATABASE IF NOT EXISTS '${MYSQL_DATABASE}';
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON '${MYSQL_DATABASE}'.* TO '${MYSQL_USER}'@'%';
-FLUSH PRIVILEGES;
-EOF
