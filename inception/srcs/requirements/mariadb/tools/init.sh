eea377a56d8952ef5ef003514e8ec43a9a858f18
Hong-CHP <93changhong@gmail.com>
2026-02-08 00:21:26 +0100

    alter_root_in_initsh

--- inception/srcs/requirements/mariadb/tools/init.sh
+++ inception/srcs/requirements/mariadb/tools/init.sh
@@ -10,6 +10,7 @@ export MYSQL_PASSWORD=$(cat "$MYSQL_PASSWORD_FILE")
 echo "Database password loaded"
 
 cat <<EOF > /docker-entrypoint-initdb.d/init.sql
+ALTER USER 'root'@'localhost' IDENTIFIED BY '888888';
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY "${MYSQL_PASSWORD}";
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
